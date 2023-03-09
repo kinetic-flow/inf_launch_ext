8f551b73115bf74b888673dd5beef74556434d43
Minsang <54884351+minsang-github@users.noreply.github.com>
2023-03-08 21:44:31 -0800

    use release builds for testing

--- deploy.bat
+++ deploy.bat
@@ -5,4 +5,4 @@ copy /y infzoom\infzoom.ini infzoom\Release
 set outdir="G:\Games\beatmania IIDX INFINITAS"
 copy /y inf_launch_ext.ps1 %outdir%
 copy /y infzoom\infzoom.ini %outdir%
-copy /y infzoom\Debug\infzoom.exe %outdir%
+copy /y infzoom\Release\infzoom.exe %outdir%
